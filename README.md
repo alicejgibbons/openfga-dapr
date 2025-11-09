e42057903aa4b0a0dbcc7a680eb5017e7f88f2e2
Alice Gibbons <alice@diagrid.io>
2025-11-09 15:43:47 +0000

    Custom statuses and clean up

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This directory contains an example of using [Dapr Workflow](https://docs.dapr.io/developing-applications/building-blocks/workflow/) along with [OpenFGA](https://openfga.dev/) to create an employee onboarding workflow that simulates the process needed to onboard a user to an enterprise Auth server and give them the associated permissions.
 
 ## Prerequisites
-
+- [OpenFGA CLI](https://github.com/openfga/cli?tab=readme-ov-file#installation)
 - [Dapr CLI and initialized environment](https://docs.dapr.io/getting-started)
 - [Install Python 3.9+](https://www.python.org/downloads/)
 
@@ -17,7 +17,38 @@ pip3 install -r requirements.txt
 
 ## Setup OpenFGA Server locally
 
-See [Setup OpenFGA with Docker](https://openfga.dev/docs/getting-started/setup-openfga/docker).
+1. See [Setup OpenFGA with Docker](https://openfga.dev/docs/getting-started/setup-openfga/docker).
+
+```
+make dev-setup
+
+Starting OpenFGA...
+80709f850f5fbbe0b00dd6d0a3b194d72049fbc58593e89182ee9e363a5f1d23
+OpenFGA started on:
+  - HTTP API: http://localhost:8080
+  - gRPC API: localhost:8081
+  - Playground: http://localhost:3000
+Waiting for OpenFGA to be ready...
+Importing OpenFGA store...
+
+✓ Store imported successfully!
+  Store ID: 01K9FTEV5JFSSDX6TQXRDN51N0
+  Model ID: 01K9FTEV5N47EE7N16T7BMXVCJ
+  Configuration saved to: .env
+
+
+✓ Development environment ready!
+  - OpenFGA: http://localhost:8080
+  - Playground: http://localhost:3000/playground
+```
+
+
+2. Create org
+
+```
+curl --request POST --url 'http://localhost:8080/organizations/?user_id=alice' --header 'content-type: application/json' --data '{"name": "acme","description": "A demo organization"}'
+```
+
 
 ## Run the User onboarding workflow
 
@@ -26,7 +57,7 @@ Each of the examples in this directory can be run directly from the command line
 This example demonstrates how to chain "activity" tasks together in a workflow. You can run this sample using the following command:
 
 ```sh
-dapr run --app-id user_onboarding_workflow --dapr-grpc-port 50001 --dapr-http-port 3500 --app-port 6005 --resources-path ./resources -- python3 user_onboarding.py
+dapr run --app-id create_resource_workflow --dapr-grpc-port 50001 --dapr-http-port 3500 --resources-path ./resources -- python workflows.py
 ```
 
 The output of this example should look like this:
--- demo/workflows.py
+++ demo/workflows.py
@@ -12,6 +12,7 @@
 
 import uuid
 from datetime import datetime
+from dataclasses import dataclass
 
 import logging
 from datetime import datetime, timedelta
@@ -32,6 +33,16 @@ wfr = WorkflowRuntime()
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("workflows")
 
+@dataclass
+class ResourceRequest:
+    """Resource request object containing openfga request properties"""
+    user_id: str
+    name: str
+    resource_type: str
+    organization_id: str
+    granted: bool = False
+    resource_id: str = ""
+
 retry_policy = RetryPolicy(
     first_retry_interval=timedelta(seconds=1),
     max_number_of_attempts=3,
@@ -43,58 +54,87 @@ retry_policy = RetryPolicy(
 
 @wfr.workflow(name="create_resource_workflow")
 def create_resource_workflow(ctx: DaprWorkflowContext, wf_input: dict):
+    req = ResourceRequest(
+        user_id=wf_input.get("user_id"),
+        name=wf_input.get("name"),
+        resource_type=wf_input.get("resource_type"),
+        organization_id=wf_input.get("organization_id"),
+    )
+    
+    logger.info(f'Starting create resource workflow for access request for user: {req.user_id} on resource: {req.name}')
     try:
         allowed = yield ctx.call_activity(
             check_permission_on_org,
             input={
-                "user_id": wf_input.get("user_id"),
-                "organization_id": wf_input.get("organization_id"),
+                "user_id": req.user_id,
+                "organization_id": req.organization_id,
             },
         )
 
-        if not allowed:
-            raise Exception(
-                "User does not have permission to add resource to organization"
-            )
+        if not allowed:  # in this case we should have a manual permission check
+            ctx.set_custom_status("Insufficient user permissions on organization")
+            logger.info(f'perms not allowed')
+            yield ctx.call_activity(approver_manual_override, input=req.user_id)
+            approved_event = ctx.wait_for_external_event('manual_override_approved')
+            timeout_event = ctx.create_timer(timedelta(hours=24))
+            winner = yield when_any([approved_event, timeout_event])
+            if winner == approved_event:
+                allowed = True
+                logger.info(f'Admin permissions have been: {result2} for user id:{wf_input}')
+                ctx.set_custom_status("Manual override - approved user permissions on organization")
+            else: 
+                raise Exception("User has insufficient permissions to add resource in organization")
+
+        logger.info(f'User permission check passed: {allowed}')
+        ctx.set_custom_status("User permissions confirmed")
 
         resource = yield ctx.call_activity(
             create_resource,
             input={
-                "user_id": wf_input.get("user_id"),
-                "organization_id": wf_input.get("organization_id"),
+                "user_id": req.user_id,
+                "organization_id": req.organization_id,
             },
         )
 
         logger.info(f"Resource created successfully: {resource}")
+        ctx.set_custom_status(f"Resource {resource} created successfully")
+        req.resource_id = resource.get("id")
+        ## ideally workflow would fail here to showcase dual write problem
 
         assigned = yield ctx.call_activity(
             assign_resource_to_organization,
             input={
-                "resource_id": resource.get("id"),
-                "organization_id": wf_input.get("organization_id"),
+                "resource_id": req.resource_id,
+                "organization_id": req.organization_id,
             },
         )
 
         logger.info(f"Resource assigned to organization: {assigned}")
+        ctx.set_custom_status(f"Resource assigned to organization: {assigned}")
+        req.granted = assigned
     except Exception as e:
-        yield ctx.call_activity(error_handler, input=str(e))
-        raise
+        if req.resource_id != "":
+            # TODO: Add compensating logic here
+            yield ctx.call_activity(error_handler, input=str(e))
+        return req.granted
 
-    return resource
+    return req.granted
 
 
 @wfr.activity(name="check_permission_on_org")
 def check_permission_on_org(ctx: DaprWorkflowContext, input: dict):
     user_id = input.get("user_id")
     organization_id = input.get("organization_id")
-    logger.info(
-        f"check permission on org activity called: user_id={user_id}, organization_id={organization_id}"
-    )
+    logger.info(f"Checked permission on org for user_id={user_id}, organization_id={organization_id}")
 
     return authz_service.check_permission_on_org(
         user_id, "can_add_resource", organization_id
     )
 
+@wfr.activity(name='approver_manual_override')
+def approver_manual_override(ctx, input):
+    logger.info(f'Notifying approver to manual override permissions denial. User id: {input}')
+    # TODO maybe have a ui here to approve?
 
 @wfr.activity(name="create_resource")
 def create_resource(ctx: DaprWorkflowContext, input: dict):
@@ -122,10 +162,10 @@ def create_resource(ctx: DaprWorkflowContext, input: dict):
 
         # Return the created resource
         return {
-            "id": resource_db.id,
-            "name": resource_db.name,
-            "resource_type": resource_db.resource_type,
-            "organization_id": resource_db.organization_id,
+            "id": resource_db.id
+            # "name": resource_db.name,
+            # "resource_type": resource_db.resource_type,
+            # "organization_id": resource_db.organization_id,
         }
 
 
@@ -140,4 +180,4 @@ def assign_resource_to_organization(ctx: DaprWorkflowContext, input: dict):
 @wfr.activity
 def error_handler(ctx, error):
     logger.info(f"Executing error handler: {error}.")
-    # Rollback permissions in the openfga server
+    # TODO: Add compensating logic here
